f5a4a47a1e9b73b81dc5341a576020823d2289f5
Paul Priest <josephplpriest@gmail.com>
2023-12-02 18:08:50 -0500

    part 1 added more comments

--- rust/day02/src/bin/part1.rs
+++ rust/day02/src/bin/part1.rs
@@ -1,11 +1,13 @@
 use regex::Regex;
-use std::collections::HashMap;
 use std::cmp;
+use std::collections::HashMap;
 
+// Generic function to read in text
 fn get_input() -> String {
     include_str!("input.txt").to_string()
 }
 
+// get the game id via regex
 fn get_game_id(hay: &str) -> u8 {
     let re = Regex::new(r"(\d+)").unwrap();
     let result = re.find(hay).unwrap();
@@ -14,54 +16,61 @@ fn get_game_id(hay: &str) -> u8 {
 }
 
 fn main() {
-    let input = get_input();
-    let input_lines = input.lines();
+    let input: String = get_input();
+    let input_lines: std::str::Lines<'_> = input.lines();
 
     let mut id_counter: i32 = 0;
 
     for line in input_lines {
+        // Rust has nice functional programming support and lambdas (like the code on the next line after 'map')
+
         let separated: Vec<String> = line.split(": ").map(|x| x.to_string()).collect();
-        let game_num = get_game_id(&separated[0]);
-        let game_str = &separated[1];
+        let game_num: u8 = get_game_id(&separated[0]);
 
-        // let games = game_str.split("; ");
-        println!("{game_num}");
+        // get the rest of the line with the game info
+        let game_str: &String = &separated[1];
 
+        // Rust's version of a python dictionary, not too different though you have to specify var types
         let mut max_colors: HashMap<String, u8> = HashMap::from([
             ("red".to_string(), 0),
             ("green".to_string(), 0),
-            ("blue".to_string(), 0)
+            ("blue".to_string(), 0),
         ]);
 
-        let valid_colors: Vec<String> = max_colors.keys().map(|k|k.clone()).collect();
+        // More functional code and rust lambda
+        let valid_colors: Vec<String> = max_colors.keys().map(|k| k.clone()).collect();
 
         let max_values: HashMap<String, u8> = HashMap::from([
             ("red".to_string(), 12),
             ("green".to_string(), 13),
-            ("blue".to_string(), 14)
+            ("blue".to_string(), 14),
         ]);
 
+        // do a regex for each color over the entire line
         for color in &valid_colors {
             let search_string = r"(\d+ ".to_owned() + &color + ")";
             let re = Regex::new(&search_string).unwrap();
             let hay = game_str;
+
+            // get an iterable of each match ie. ["7 red", "5 red", "10 red"]
             let matches: Vec<String> = re
                 .find_iter(hay)
-                .filter_map(|draws| draws.as_str().parse().ok()
-                )
+                .filter_map(|draws| draws.as_str().parse().ok())
                 .collect();
+
+            // iterate through matches and add the largest value to the hashmap
             for m in matches {
                 let re = Regex::new(r"(\d+)").unwrap();
                 let count = re.find(&m).unwrap().as_str().parse::<u8>().unwrap();
                 max_colors.insert(color.clone(), cmp::max(count, max_colors[color]));
             }
         }
-        
+
+        // Rust version of python's list.all() to check if all color counts are less than the max allowed
         if valid_colors.iter().all(|c| max_colors[c] <= max_values[c]) {
             id_counter += game_num as i32
         }
 
         println!("{id_counter}");
-
     }
 }
